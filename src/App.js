fd86a0f091d3d1fd65e8a31643199bb8f82571d6
Jackson Lorran <computadordb@gmail.com>
2023-08-29 10:42:19 -0300

    Mostrando os produtos na tabela

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-
+import React, { useEffect, useState } from "react";
 import './App.css';
 import Formulario from './Formulario';
 import Tabela from './Tabela';
 
 function App() {
+  //useState
+  const [btnCadastrar,setBtnCadastrar]=useState(true);
+  const[produtos,setProdutos]=useState({});
+  //UseEffect
+  useEffect(()=>{
+    fetch("http://localhost:8080/listar").then(retorno=>retorno.json())
+    .then(retorn_convertido=>setProdutos(retorn_convertido))
+  },[]);
+  //retorno
   return (
-    <div className="App">
-      <Formulario />
-      <Tabela />
+    <div>
+      <Formulario botao={btnCadastrar} />
+      <Tabela vetor={produtos}/>
     </div>
   );
 }
