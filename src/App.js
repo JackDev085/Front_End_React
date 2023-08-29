64af0ce7736c4dfcea358239902e5afe95dad81e
Jackson Lorran <computadordb@gmail.com>
2023-08-29 11:35:54 -0300

    Método cadastrar

--- src/App.js
+++ src/App.js
@@ -4,18 +4,56 @@ import Formulario from './Formulario';
 import Tabela from './Tabela';
 
 function App() {
+
+  //objeto produto
+  const produto = {
+
+    codigo :0,
+    nome :'',
+    marca:''
+  }
   //useState
   const [btnCadastrar,setBtnCadastrar]=useState(true);
-  const[produtos,setProdutos]=useState({});
+  const[produtos,setProdutos]=useState([]);
+  const[objProduto, setObjProduto]=useState(produto);
+
   //UseEffect
   useEffect(()=>{
     fetch("http://localhost:8080/listar").then(retorno=>retorno.json())
-    .then(retorn_convertido=>setProdutos(retorn_convertido))
+    .then(retorno_convertido=>setProdutos(retorno_convertido))
   },[]);
+
+  //obtendo os dados do formulário
+  const aoDigitar=(e)=>{
+    setObjProduto({...objProduto,[e.target.name]:e.target.value});
+  }
+
+  //Método cadastrar produto
+  const cadastrar = () => {
+    fetch('http://localhost:8080/cadastrar',{
+      method:'post',
+      body:JSON.stringify(objProduto),
+      headers:{
+        'Content-type':'application/json',
+        'Accept':'application/json'
+      }
+    })
+    .then(retorno => retorno.json())
+    .then(retorno_convertido=>{
+
+      if(retorno_convertido.mensagem !== undefined){
+        alert(retorno_convertido.mensagem);
+      }else{
+        setProdutos([...produtos,retorno_convertido]);
+        alert('Produto cadastrado com sucesso!');
+      }
+  })
+}
   //retorno
   return (
     <div>
-      <Formulario botao={btnCadastrar} />
+      <Formulario botao={btnCadastrar} eventoTeclado={aoDigitar}cadastrar={cadastrar}
+      />
       <Tabela vetor={produtos}/>
     </div>
   );
--- src/Formulario.js
+++ src/Formulario.js
@@ -1,12 +1,12 @@
-function Formulario({botao}){
+function Formulario({botao,eventoTeclado,cadastrar}){
     return(
         <form>
-            <input type='text' placeholder='Nome' className='form-control'></input>
-            <input type='text' placeholder='Marca'className='form-control'></input>
+            <input type='text' onChange={eventoTeclado} name= 'nome' placeholder='Nome' className='form-control'></input>
+            <input type='text' onChange={eventoTeclado} name='marca' placeholder='Marca'className='form-control'></input>
             {
                 botao
                 ?
-                <input type='button' value='Cadastrar'className='btn btn-primary'></input>
+                <input type='button' value='Cadastrar' onClick={cadastrar} className='btn btn-primary'></input>
                 :
                 <div>
                     <input type='button' value='Alterar'className='btn btn-warning'></input>
